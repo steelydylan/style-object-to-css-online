1632c107e21e3f0c6063119d47b01636030eea53
steelydylan <ess_president@me.com>
2020-12-02 16:10:38 +0900

    style

--- index.tsx
+++ index.tsx
@@ -4,6 +4,22 @@ import cssBeautify from "cssbeautify"
 import csso from 'csso'
 import objectStyle from './object-style'
 
+const placeholder = `{
+  drawer: {
+    position: 'relative',
+    height: '100%',
+    width: 72,
+    flex: 'initial',
+    backgroundColor: '#fff',
+    boxShadow: '0px 1px 3px 0 rgba(0, 0, 0, 0.15)',
+    borderRight: 'none',
+    zIndex: 11,
+    '& .MuiListItemText-root': {
+      display: 'none',
+    },
+  }
+}`
+
 const Converter = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
   const [result, setResult] = useState("")
@@ -14,10 +30,12 @@ const Converter = () => {
   }
   return (<div className="flex">
     <div className="flex-item">
-      <textarea className="source" ref={textareaRef} />
+      CSS Object
+      <textarea className="source" ref={textareaRef} placeholder={placeholder} />
       <button className="btn" onClick={handleClick}>変換</button>
     </div>
     <div className="flex-item">
+      CSS
       <textarea className="target" readOnly value={result} />
     </div>
   </div>)
