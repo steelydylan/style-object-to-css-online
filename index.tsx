e05748ea6e5c804a092ff88b7d9ec37c50d7ddb2
steelydylan <ess_president@me.com>
2020-12-02 15:17:19 +0900

    first commit

---
+++ index.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useRef } from "react";
+import { render } from "react-dom"
+import objectStyle from 'object-style'
+
+const Converter = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const [result, setResult] = useState("")
+  const handleClick = () => {
+    const { value } = textareaRef.current
+    const { css } = objectStyle(eval(`(${value})`))
+    setResult(css)
+  }
+  return (<div className="flex">
+    <div className="flex-item">
+      <textarea className="source" ref={textareaRef} />
+      <button className="btn" onClick={handleClick}>変換</button>
+    </div>
+    <div className="flex-item">
+      <textarea className="target" readOnly value={result} />
+    </div>
+  </div>)
+}
+
+render(<Converter />, document.getElementById("app"))
